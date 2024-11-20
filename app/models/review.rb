ad570cd717d8071b14f446f095a33ab52b342b85
Maira Almeida <mairasenna@gmail.com>
2024-11-20 14:55:50 -0300

    rails new

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: 0..5, message: 'must be between 0 and 5' }
+end
